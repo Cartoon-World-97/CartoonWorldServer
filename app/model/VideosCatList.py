347225f432dd7d7c9689b900436f50711c622700
Cartoon-World-97 <cartoonworld.server@gmail.com>
2026-01-18 20:40:40 +0530

    Refactor VideosCatList model attributes

--- app/model/VideosCatList.py
+++ app/model/VideosCatList.py
@@ -1,11 +1,21 @@
-from datetime import date, datetime
+from datetime import datetime
 from . import db
 
 class VideosCatList(db.Model):
     __tablename__ = 'videos_cat_list'
-    SL = db.Column(db.Integer,primary_key=True,autoincrement=True)
-    Name = db.Column(db.String,nullable=False)
-    Category_ID = db.Column(db.String,db.ForeignKey("video_category.Category_ID"),nullable=False)
+
+    SL = db.Column(db.Integer, primary_key=True, autoincrement=True)
+
+    Name = db.Column(db.String(150), nullable=False)
+
+    Category_ID = db.Column(
+        db.String(50),
+        db.ForeignKey("video_category.Category_ID"),
+        nullable=False,
+        index=True
+    )
+
     Is_title_Card = db.Column(db.SmallInteger, default=0, nullable=False)
     Status = db.Column(db.SmallInteger, default=1, nullable=False)
-    Created =  db.Column(db.DateTime, default=datetime.utcnow, nullable=False)
+
+    Created = db.Column(db.DateTime, default=datetime.utcnow, nullable=False)
