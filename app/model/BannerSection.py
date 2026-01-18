528f2f3d1a0499a52f852ead3501faf022c7238f
Cartoon-World-97 <cartoonworld.server@gmail.com>
2026-01-18 20:44:20 +0530

    Refactor Video_ID column definition in BannerSection

--- app/model/BannerSection.py
+++ app/model/BannerSection.py
@@ -1,12 +1,20 @@
-from datetime import datetime,date
+from datetime import datetime, date
 from . import db
 
 class BannerSection(db.Model):
     __tablename__ = 'banner_section'
 
     SL = db.Column(db.Integer, primary_key=True, autoincrement=True)
-    Video_ID = db.Column(db.String,db.ForeignKey("video_master.Video_ID"),nullable=False)
+
+    Video_ID = db.Column(
+        db.String(50),
+        db.ForeignKey("video_master.Video_ID"),
+        nullable=False,
+        index=True
+    )
+
     Default = db.Column(db.SmallInteger, default=0, nullable=False)
     Active_sts = db.Column(db.SmallInteger, default=1, nullable=False)
-    Date = db.Column(db.Date, default=date.today)	
+
+    Date = db.Column(db.Date, default=date.today)
     Time = db.Column(db.Time, default=lambda: datetime.now().time())
