93815a31b152cdab026391fc43067b2f9298354e
Cartoon-World-97 <cartoonworld.server@gmail.com>
2026-01-18 23:19:42 +0530

    Update __init__.py

--- app/__init__.py
+++ app/__init__.py
@@ -24,8 +24,6 @@ def create_app():
     mail = Mail(app)
     JWTManager(app)
     db.init_app(app)
-    with app.app_context():
-        db.create_all()
 
     S = URLSafeTimedSerializer(app.config['SECRET_KEY'])
 
