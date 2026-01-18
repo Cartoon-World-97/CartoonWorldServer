46d574b1b988874fa690f4d1f9657d844f8dd2db
Cartoon-World-97 <cartoonworld.server@gmail.com>
2026-01-18 20:38:20 +0530

    Refactor column types and add indexes in playlistsMaster

--- app/model/playlistsMaster.py
+++ app/model/playlistsMaster.py
@@ -1,11 +1,13 @@
 from datetime import date, datetime
-from . import db 
+from . import db
 
 class playlistsMaster(db.Model):
     __tablename__ = 'playlists_master'
 
     SL = db.Column(db.Integer, primary_key=True, autoincrement=True)
-    Sub_ID = db.Column(db.String(150), nullable=False)
-    Video_ID = db.Column(db.String(150), nullable=False)
-    Date = db.Column(db.Date, default=date.today)	
+
+    Sub_ID = db.Column(db.String(50), nullable=False, index=True)
+    Video_ID = db.Column(db.String(50), nullable=False, index=True)
+
+    Date = db.Column(db.Date, default=date.today)
     Time = db.Column(db.Time, default=lambda: datetime.now().time())
