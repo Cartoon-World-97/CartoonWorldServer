848719b16f0387cad4913e49b5ae945e1fb6c9de
Cartoon-World-97 <cartoonworld.server@gmail.com>
2026-01-18 20:36:13 +0530

    Update videoCategory.py

--- app/model/videoCategory.py
+++ app/model/videoCategory.py
@@ -4,9 +4,9 @@ from . import db
 class videoCategory(db.Model):
     __tablename__ = 'video_category'
     SL = db.Column(db.Integer,primary_key=True,autoincrement=True)
-    Category_ID = db.Column(db.String,nullable=False,unique=True)
-    Category_Name = db.Column(db.String,nullable=False,unique=True)
-    icon = db.Column(db.String,nullable=False)
-    Description = db.Column(db.String,nullable=False)
+    Category_ID = db.Column(db.String(150),nullable=False,unique=True)
+    Category_Name = db.Column(db.String(150),nullable=False,unique=True)
+    icon = db.Column(db.String(150),nullable=False)
+    Description = db.Column(db.String(150),nullable=False)
 
     videos = db.relationship("videoMaster", back_populates="category")
